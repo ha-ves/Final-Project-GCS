e68970e0faa574b7e73695b853fae14bb8a28845
Haves Irfansyah <havesirfan@ce.student.pens.ac.id>
2021-10-12 10:20:16 +0700

    Add project files.

---
+++ TugasAkhir_GCS.LinuxGTK/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.GTK;
+
+namespace TugasAkhir_GCS.LinuxGTK
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Gtk.Application.Init();
+            Forms.Init();
+
+            var app = new App();
+            var window = new FormsWindow();
+            window.LoadApplication(app);
+            window.SetApplicationTitle("Test Xamarin GTK#");
+            window.Show();
+
+            Gtk.Application.Run();
+        }
+    }
+}
---
+++ TugasAkhir_GCS/TugasAkhir_GCS/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TugasAkhir_GCS
+{
+    public partial class MainPage : ContentPage, INotifyPropertyChanged
+    {
+        string _testString = "Testing 1 2 3 4";
+        public string TestString { get { return _testString; } set { _testString = value; OnPropertyChanged("TestString"); } }
+
+        public ImageSource ImgUsed { get { return ImageSource.FromResource("PigeonMobile_Xamarin_Cs.Resources.pigeon.png", typeof(App).Assembly); } }
+
+        public MainPage()
+        {
+            BindingContext = this;
+            InitializeComponent();
+        }
+    }
+}
