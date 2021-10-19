84fea3b836c2e30f950e7112c9a9037d74554ca1
Haves Irfansyah <havesirfan@ce.student.pens.ac.id>
2021-10-19 14:01:03 +0700

    working aes cbc encrypt/decrypt

--- TugasAkhir_GCS/TugasAkhir_GCS/MainPage.xaml.cs
+++ TugasAkhir_GCS/TugasAkhir_GCS/MainPage.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net.Sockets;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -15,10 +19,75 @@ namespace TugasAkhir_GCS
 
         public ImageSource ImgUsed { get { return ImageSource.FromResource("PigeonMobile_Xamarin_Cs.Resources.pigeon.png", typeof(App).Assembly); } }
 
+        Aes aes;
+        ICryptoTransform encryptor, decryptor;
+
+        TcpClient tcpClient;
+
         public MainPage()
         {
             BindingContext = this;
             InitializeComponent();
+
+            
+        }
+
+        private void AES_Method()
+        {
+            aes = Aes.Create();
+            aes.Mode = CipherMode.CBC;
+            aes.Key = Encoding.ASCII.GetBytes("testing123456789");
+            aes.IV = Encoding.ASCII.GetBytes("testing123456789");
+
+            encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+            decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            string plain = "test1234";
+            string cipher64 = "";
+
+            string cipher = "+kHv/IeW5ITSt2wXMEBGhnuc1fTLV6KbFQ0JIaVGxTc=";
+            string decrypted = "";
+
+            // Create the streams used for encryption.
+            using (MemoryStream msEncrypt = new MemoryStream())
+            {
+                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                    {
+                        //Write all data to the stream.
+                        swEncrypt.Write(plain);
+                    }
+                    Debug.WriteLine("");
+                    Debug.WriteLine($"Encrypted Base64 : {Convert.ToBase64String(msEncrypt.ToArray())}");
+                    Debug.Write($"Encrypted HEX : ");
+                    msEncrypt.ToArray().ToList().ForEach(item => Debug.Write(item.ToString("x2")));
+                    Debug.WriteLine("");
+                }
+            }
+
+            // Create the streams used for decryption.
+            using (MemoryStream msDecrypt = new MemoryStream(Convert.FromBase64String(cipher)))
+            {
+                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                {
+                    using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                    {
+
+                        // Read the decrypted bytes from the decrypting stream
+                        // and place them in a string.
+                        decrypted = srDecrypt.ReadToEnd();
+                        Debug.WriteLine("\n\nDecoded Base64 HEX : ");
+                        Convert.FromBase64String(cipher).ToList().ForEach(item => Debug.Write(item.ToString("x2")));
+                        Debug.WriteLine("");
+                        Debug.WriteLine($"Decrypted : {decrypted}");
+                        Debug.WriteLine("");
+                    }
+                }
+            }
         }
     }
 }
